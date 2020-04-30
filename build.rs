48ac1e97b3aaa04cfaef368c22188959a854af14
Avi Weinstock <aweinstock314@gmail.com>
2020-04-29 21:22:03 -0400

    Add OS detection to build.rs for determining which C++ library to use.

--- build.rs
+++ build.rs
@@ -4,8 +4,15 @@ use std::env;
 const HEADER_NAME: &'static str = "fswatch/libfswatch/src/libfswatch/c/libfswatch.h";
 const LIBRARY_PATH: &'static str = "fswatch/libfswatch/src/libfswatch/.libs/";
 
+#[cfg(any(target_os="macos", target_os="freebsd"))]
+const CPP_LIB: &'static str = "c++";
+#[cfg(target_os="linux")]
+const CPP_LIB: &'static str = "stdc++";
+// no fallthrough case: to support more OSes, add their default C++ library here
+// an error about CPP_LIB being undefined should lead right here, but the linker errors could be much more confusing
+
 fn main() {
-    for lib in &["fswatch", "stdc++"] {
+    for lib in &["fswatch", CPP_LIB] {
         println!("cargo:rustc-link-lib=static={}", lib);
     }
     println!("cargo:rerun-if-changed={}", HEADER_NAME);
